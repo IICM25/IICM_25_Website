e944c45bfe4e3802ac8aecfc7c23d395c623634e
rpyaduvanshi950 <rpyaduvanshi950@gmail.com>
2025-11-13 02:14:40 +0530

    update the small screen navbar

--- src/components/layout/MobileDrawer.tsx
+++ src/components/layout/MobileDrawer.tsx
@@ -9,7 +9,7 @@ interface MobileDrawerProps {
 }
 
 const MobileDrawer: React.FC<MobileDrawerProps> = ({ isOpen, onClose }) => {
-  const [isGalleryOpen, setIsGalleryOpen] = useState(false);
+  
 
   return (
     <>
@@ -34,154 +34,34 @@ const MobileDrawer: React.FC<MobileDrawerProps> = ({ isOpen, onClose }) => {
         `}
       >
         <ul className="flex flex-col items-center list-none p-0">
-          {/* Register */}
           <li className="my-2">
-            <a
-              href="https://events.antaragni.in"
-              target="_blank"
-              rel="noreferrer"
-              onClick={onClose}
-              className="text-white text-[22px] hover:text-white"
-            >
-              Register
-            </a>
+            <Link href="/iitk" onClick={onClose} className="text-white text-[22px] hover:text-white">
+              About IITK
+            </Link>
           </li>
 
-          {/* CA Program */}
           <li className="my-2">
-            <a
-              href="https://ca.antaragni.in"
-              target="_blank"
-              rel="noreferrer"
-              onClick={onClose}
-              className="text-white text-[22px] hover:text-white"
-            >
-              CA Program
-            </a>
-          </li>
-
-          {/* Roadtrips Dropdown */}
-          <li className="my-2">
-  <a
-    href="https://events.antaragni.in/roadtrips"
-    target="_blank"
-    rel="noreferrer"
-    onClick={onClose}
-    className="text-white text-[22px] cursor-pointer hover:text-white"
-  >
-    Roadtrips
-  </a>
-</li>
-
-
-          {/* Gallery Dropdown */}
-          <li className="relative my-2">
-            <span
-              className="text-white text-[22px] cursor-pointer hover:text-white"
-              onClick={() => setIsGalleryOpen(!isGalleryOpen)}
-            >
+            <Link href="/gallery" onClick={onClose} className="text-white text-[22px] hover:text-white">
               Gallery
-            </span>
-            <ul
-              className={`
-                ${isGalleryOpen ? "block" : "hidden"}
-                absolute -left-[155%] -top-[200%]
-                bg-[#333] rounded-lg shadow-md px-4 py-2
-              `}
-            >
-              <li>
-                <Link
-                  href="/gallery"
-                  onClick={onClose}
-                  className="block text-white text-xs py-1 hover:text-white"
-                >
-                  Gallery
-                </Link>
-              </li>
-              <li>
-                <Link
-                  href="/brochure"
-                  onClick={onClose}
-                  className="block text-white text-xs py-1 hover:text-white"
-                >
-                  Brochure
-                </Link>
-              </li>
-              <li>
-                <Link
-                  href="/media"
-                  onClick={onClose}
-                  className="block text-white text-xs py-1 hover:text-white"
-                >
-                  Media
-                </Link>
-              </li>
-            </ul>
+            </Link>
           </li>
 
-          {/* Internal Links */}
-          {/* <li className="my-2">
-            <Link
-              href="/hof"
-              onClick={onClose}
-              className="text-white text-[22px] hover:text-white"
-            >
-              Hall of Fame
-            </Link>
-          </li> */}
           <li className="my-2">
-            <Link
-              href="/merch"
-              onClick={onClose}
-              className="text-white text-[22px] hover:text-white"
-            >
-              Merchandise
+            <Link href="/contact" onClick={onClose} className="text-white text-[22px] hover:text-white">
+              Contact
             </Link>
           </li>
+
           <li className="my-2">
-            <Link
-              href="/schedule"
+            <a
+              href="https://drive.google.com/file/d/1VUxhRSf0R2xDKwhzhKKDB2_uktCDeV6M/view?usp=drive_open"
+              target="_blank"
+              rel="noreferrer"
               onClick={onClose}
               className="text-white text-[22px] hover:text-white"
             >
-              Schedule
-            </Link>
-          </li>
-          <li className="my-2">
-            <Link
-              href="/attractions"
-              onClick={onClose}
-              className="text-white text-[22px] hover:text-white"
-            >
-              Star Attractions
-            </Link>
-          </li>
-          <li className="my-2">
-            <Link
-              href="/coreteam"
-              onClick={onClose}
-              className="text-white text-[22px] hover:text-white"
-            >
-              Core Team
-            </Link>
-          </li>
-          <li className="my-2">
-            <Link
-              href="/queries"
-              onClick={onClose}
-              className="text-white text-[22px] hover:text-white"
-            >
-              Queries
-            </Link>
-          </li>
-          <li className="my-2">
-            <Link
-              href="/sponsors"
-              onClick={onClose}
-              className="text-white text-[22px] hover:text-white"
-            >
-              Sponsors
-            </Link>
+              RuleBook
+            </a>
           </li>
         </ul>
       </div>
